5f59e4f6faac88666d6296a91d46920296869cff
Ákos Demján <akos.demjan@gmail.com>
2020-03-08 14:55:24 +0100

    Added new tasks

--- week-01/day-4/src/FizzBuzz.java
+++ week-01/day-4/src/FizzBuzz.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3) == 0 && (i % 5) == 0) {
+                System.out.println("FizzBuzz"); // OR (i % 15 == 0)
+            } else  if ((i % 5) == 0) {
+                System.out.println("Buzz");
+            } else if ((i % 3) == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
--- week-01/day-4/src/MultiplicationTable.java
+++ week-01/day-4/src/MultiplicationTable.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 1; i <=10; i++) {
+            System.out.println(i + " * " + n + " = " + i*n);
+
+        }
+    }
 }

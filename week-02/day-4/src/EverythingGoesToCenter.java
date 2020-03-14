bacee712ba1050acd58a232688aadbadedeb82d4
Ákos Demján <akos.demjan@gmail.com>
2020-03-15 00:52:33 +0100

    Added: ConnetTheDots and StarryNight

--- week-02/day-4/src/EverythingGoesToCenter.java
+++ week-02/day-4/src/EverythingGoesToCenter.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class EverythingGoesToCenter {
+  public static void mainDraw(Graphics graphics) {
+    int distance = 20;
+    for (int i = 0; i <=HEIGHT / distance; i++) {
+      for (int j = 0; j <=WIDTH/distance; j++) {
+        if(i==0 || i==HEIGHT/ distance || j==0 || j==WIDTH/ distance)
+          lineDrawer(j*distance, i*distance, graphics);
+      }
+    }
+  }
+
+  private static void lineDrawer(int x, int y, Graphics graphics) {
+    graphics.setColor(new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+    graphics.drawLine(x, y, WIDTH / 2, HEIGHT / 2);
+  }
+
+
+  // Don't touch the code below
+  // Sorry, but I had to ;D
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
 }
--- week-02/day-4/src/StarryNight.java
+++ week-02/day-4/src/StarryNight.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class StarryNight {
+  public static void mainDraw(Graphics graphics) {
+    graphics.setColor(Color.black);
+    graphics.fillRect(0,0,WIDTH,HEIGHT);
+    int numberOfStars=300;
+    for (int i = 0; i <numberOfStars; i++) {
+      starDraw(graphics);
+    }
+  }
+
+  private static void starDraw(Graphics graphics) {
+    int padding=(int) (Math.random() * 4);
+    int color=(int) (Math.random() * 255);
+    graphics.setColor(new Color(color,color,color));
+    graphics.fillRect((int)(Math.random() * WIDTH-padding),(int)(Math.random() * HEIGHT-padding), padding, padding);
+  }
+
+  // Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 320;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    ImagePanel panel = new ImagePanel();
+    panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.add(panel);
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+    jFrame.pack();
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
 }

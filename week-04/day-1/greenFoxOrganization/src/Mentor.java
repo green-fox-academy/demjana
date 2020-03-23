67abeefc067f4d5397cd33a79048d71503ca0471
Ákos Demján <akos.demjan@gmail.com>
2020-03-24 00:08:52 +0100

    Green Fox inharitance exercise is added.

---
+++ week-04/day-1/greenFoxOrganization/src/Mentor.java
@@ -0,0 +1,25 @@
+public class Mentor extends Person {
+  String level;
+
+  public Mentor() {
+    super();
+    this.level = "intermediate";
+  }
+
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Educate brilliant junior software developers.");
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + " a " + this.age + " year old " + this.gender +
+        " " + this.level + " mentor.");
+  }
+
+}
---
+++ week-04/day-1/greenFoxOrganization/src/Sponsor.java
@@ -0,0 +1,34 @@
+public class Sponsor extends Person {
+  String company;
+  int hiredStudents;
+
+  public Sponsor() {
+    super();
+    this.company = "Google";
+    this.hiredStudents = 0;
+  }
+
+  public Sponsor(String name, int age, String gender, String company) {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+
+  @Override
+  public void introduce() {
+    System.out.println("Hi, I'm " + this.name + " a " + this.age + " year old " +
+        this.gender + " who represents " + this.company + " and hired " + this.hiredStudents +
+        " students so far.");
+  }
+
+  @Override
+  public void getGoal() {
+    System.out.println("Hire brilliant junior software developers.");
+  }
+
+  public void hire() {
+    this.hiredStudents++;
+  }
+
+
+}
